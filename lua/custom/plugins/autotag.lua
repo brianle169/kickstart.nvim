7dcc070144685e2f9715a04ec3afb0bd7c16b047
Brian Le <83758821+brianle169@users.noreply.github.com>
2023-05-11 01:57:51 +0700

    Add nvim-ts-autotag

---
+++ lua/custom/plugins/autotag.lua
@@ -0,0 +1,15 @@
+return {
+  'windwp/nvim-ts-autotag',
+  config = function()
+    require('nvim-ts-autotag').setup({
+      filetypes = {
+        'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'tsx', 'jsx',
+        'rescript',
+        'xml',
+        'php',
+        'markdown',
+        'astro', 'glimmer', 'handlebars', 'hbs'
+      }
+    })
+  end
+}
