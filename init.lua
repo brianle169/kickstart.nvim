42589757ed8fcb504dbd8408a785a3ac295353be
Brian Le <83758821+brianle169@users.noreply.github.com>
2023-05-10 16:26:00 +0700

    Change colorscheme to TsuZat/Neosolarized.nvim

--- init.lua
+++ init.lua
@@ -113,10 +113,10 @@ require('lazy').setup({
   },
 
   { -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    'TsuZat/NeoSolarized.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'NeoSolarized'
     end,
   },
 
