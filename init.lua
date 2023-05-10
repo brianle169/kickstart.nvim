291ca8bbf0b35a28385349ae8d4a04287be7f082
Brian Le <83758821+brianle169@users.noreply.github.com>
2023-05-10 23:34:04 +0700

    Keymaps settings

--- init.lua
+++ init.lua
@@ -4,35 +4,6 @@
 ==================== READ THIS BEFORE CONTINUING ====================
 =====================================================================
 
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-  The goal is that you can read every line of code, top-to-bottom, and understand
-  what your configuration is doing.
-
-  Once you've done that, you should start exploring, configuring and tinkering to
-  explore Neovim!
-
-  If you don't know anything about Lua, I recommend taking some time to read through
-  a guide. One possible example:
-  - https://learnxinyminutes.com/docs/lua/
-
-  And then you can explore or search through `:help lua-guide`
-
-
-Kickstart Guide:
-
-I have left several `:help X` comments throughout the init.lua
-You should run that command and read that help section for more information.
-
-In addition, I have some `NOTE:` items throughout the file.
-These are for you, the reader to help understand what is happening. Feel free to delete
-them once you know what you're doing, but they should serve as a guide for when you
-are first encountering a few different constructs in your nvim config.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
 P.S. You can delete this when you're done too. It's your config now :)
 --]]
 -- Set <space> as the leader key
@@ -147,13 +118,14 @@ require('lazy').setup({
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons', lazy = true },
     -- See `:help lualine.txt`
     opts = {
       options = {
         icons_enabled = true,
         theme = "NeoSolarized",
-        component_separators = { left = '', right = '' },
         section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = '' },
       },
       sections = {
         lualine_a = { 'mode' },
@@ -295,6 +267,16 @@ vim.o.completeopt = 'menuone,noselect'
 vim.o.termguicolors = true
 
 -- [[ Basic Keymaps ]]
+
+-- Navigation keymaps - Inspired by NvChad
+-- INSERT MODE
+vim.keymap.set('i', '<C-b>', '<ESC>^i')  -- beginning of lines
+vim.keymap.set('i', '<C-e>', '<End>')    -- end of line
+vim.keymap.set('i', 'C-h>', '<Left>')    -- navigate in insert mode
+vim.keymap.set('i', 'C-j>', '<Down>')    -- navigate in insert mode
+vim.keymap.set('i', 'C-k>', '<Up>')      -- navigate in insert mode
+vim.keymap.set('i', 'C-l>', '<Right>')   -- navigate in insert mode
+
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
