c50d3fa0e1bb3908670f88e384ea0778a4694d13
Brian Le <83758821+brianle169@users.noreply.github.com>
2023-05-13 19:18:21 +0700

    Change dashboard highlight group, and remove set colorscheme option

--- lua/custom/plugins/dashboard-nvim.lua
+++ lua/custom/plugins/dashboard-nvim.lua
@@ -7,7 +7,8 @@ return {
       theme = 'doom',
       disable_move = true,
       config = {
-        header = { '',
+        header = {
+          '',
           '                                                       ',
           '                                                       ',
           '                                                       ',
@@ -24,91 +25,96 @@ return {
           '                                                       ',
           '                                                       ',
           '                                                       ',
-          '', },
+          '',
+        },
         center = {
           {
             icon = '󰉋   ',
-            icon_hl = 'Keyword',
+            icon_hl = 'String',
             desc = 'Open File Tree',
             desc_hl = 'Function',
             key = 'n',
             key_hl = 'Boolean',
-            action = function() vim.cmd('Neotree toggle=true <CR>') end
+            action = function()
+              vim.cmd 'Neotree toggle=true <CR>'
+            end,
           },
           {
             icon = '󰈙   ',
-            icon_hl = 'Keyword',
+            icon_hl = 'String',
             desc = 'Search Files',
             desc_hl = 'Function',
             key = 'f',
             key_hl = 'Boolean',
-            action = function() vim.cmd('Telescope find_files') end
+            action = function()
+              vim.cmd 'Telescope find_files'
+            end,
           },
           {
             icon = '   ',
-            icon_hl = 'Keyword',
+            icon_hl = 'String',
             desc = 'Search Old Files',
             desc_hl = 'Function',
             key = '?',
             key_hl = 'Boolean',
-            action = function() vim.cmd('Telescope oldfiles') end
-          },
-          {
-            icon = '󰏘   ',
-            icon_hl = 'Keyword',
-            desc = 'Set Colorscheme',
-            desc_hl = 'Function',
-            key = 'c',
-            key_hl = 'Boolean',
-            action = function() vim.cmd('Telescope colorscheme') end
+            action = function()
+              vim.cmd 'Telescope oldfiles'
+            end,
           },
           {
             icon = '   ',
-            icon_hl = 'Keyword',
+            icon_hl = 'String',
             desc = 'Search Live Grep',
             desc_hl = 'Function',
             key = 'g',
             key_hl = 'Boolean',
-            action = function() vim.cmd('Telescope live_grep') end
+            action = function()
+              vim.cmd 'Telescope live_grep'
+            end,
           },
           {
             icon = '󰌋   ',
-            icon_hl = 'Keyword',
+            icon_hl = 'String',
             desc = 'Search Keymaps',
             desc_hl = 'Function',
             key = 'm',
             key_hl = 'Boolean',
-            action = function() vim.cmd('Telescope keymaps') end
+            action = function()
+              vim.cmd 'Telescope keymaps'
+            end,
           },
           {
             icon = '   ',
-            icon_hl = 'Keyword',
+            icon_hl = 'String',
             desc = 'View Lazy Plugins',
             desc_hl = 'Function',
             key = 'l',
             key_hl = 'Boolean',
-            action = function() vim.cmd('Lazy') end
+            action = function()
+              vim.cmd 'Lazy'
+            end,
           },
 
           {
             icon = '   ',
-            icon_hl = 'Keyword',
+            icon_hl = 'String',
             desc = 'Quit NeoVIM (Yes you can exit VIM)',
             desc_hl = 'Function',
             key = 'q',
             key_hl = 'Boolean',
-            action = function() vim.cmd('q') end
-
-          }
+            action = function()
+              vim.cmd 'q'
+            end,
+          },
         },
         footer = {
           '                                                       ',
           '                                                       ',
           '                                                       ',
-          ' Welcome to the work station of Binobo '
-        } --your footer
-      }
+          ' Welcome to the work station of Binobo ',
+        }, --your footer
+      },
     }
   end,
-  dependencies = { { 'nvim-tree/nvim-web-devicons' } }
+  dependencies = { { 'nvim-tree/nvim-web-devicons' } },
 }
